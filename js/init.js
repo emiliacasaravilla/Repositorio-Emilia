fd1ee842f8adf309cb77153a880ccfcc7a6fbbc2
emiliacasaravilla <emicasaravilla@gmail.com>
2022-09-23 12:18:20 -0300

    le agregue margenes y revise unas cositas de alertas

--- js/init.js
+++ js/init.js
@@ -43,12 +43,15 @@ let getJSONData = function(url){
 
 
 
-function ponerNombreUsuario (){
-   let htmlContentToAppend = localStorage.getItem('nombreUsuario');
-  document.getElementById("nombreDeUsuario").innerHTML = htmlContentToAppend;
-}
+//function ponerNombreUsuario (){
+  // let htmlContentToAppend = localStorage.getItem('nombreUsuario');
+  //document.getElementById("nombreDeUsuario").innerHTML = htmlContentToAppend;
+//}
 
-ponerNombreUsuario();
+//ponerNombreUsuario();
+
+//ESTA FUNCION ERA DE UNA ENTRGA ANTERIOR PERO LA OCULTE PORQUE LO DE ABAJO DEL MENU
+//DESPLEGABLE LO DEJA INNECESARIO, YA QUE EL MENU SE ENCAGA DE GUARDAR EL NOMBRE
 
 //Creé la función que a través del getitem accede a la info guardada en el alacenamiento local
 //Agregué esa info (el mail) con en el espacio que ya estaba creado en todos los html
@@ -56,3 +59,41 @@ ponerNombreUsuario();
 //Lo hice desde init porque es un archivo que esta anexado a todos los html, así esa info aparece en todas 
 //las ventanas del sitio
 //Luego invoqué la funcion 
+
+
+function cerrarSesion(){
+  localStorage.clear('nombreUsuario');
+}
+
+
+function agregarMenuDesplegable (){
+  
+  let htmlContentToAppend = "";
+ 
+   
+  htmlContentToAppend += `
+  
+  <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+  ${localStorage.getItem('nombreUsuario')}
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="cart.html">Mi carrito</a></li>
+    <li><a class="dropdown-item" href="my-profile.html">Mi perfil</a></li>
+    <li><a class="dropdown-item" href="index.html" id="xxx" onclick="${cerrarSesion()}">Cerrar sesión</a></li>
+  </ul>
+</div>
+
+  `    
+document.getElementById("nombreDeUsuario").innerHTML = htmlContentToAppend;
+};
+
+// onclick="${cerrarSesion()}"
+
+
+
+agregarMenuDesplegable ();
+  
+//xxx.onclick = cerrarSesion();
+  
+ 
