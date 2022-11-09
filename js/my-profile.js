d32737735ca1213a06f40b30a33215b5fd4f8a4c
emiliacasaravilla <emicasaravilla@gmail.com>
2022-11-09 01:38:17 -0300

    Entrega 7 terminada!

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,95 +1,120 @@
 let email = localStorage.getItem('nombreUsuario');
 
-function ponerEmail (){
+
+function ponerDatos (){
     document.getElementById("email").value = email;
+  
+   document.getElementById("nombre").value = localStorage.getItem('nombre');
+   document.getElementById("apellido").value = localStorage.getItem('apellido');
+   document.getElementById("segundoApellido").value = localStorage.getItem('segundoApellido');
+   document.getElementById("segundoNombre").value = localStorage.getItem('segundoNombre');
+   document.getElementById("telefono").value = localStorage.getItem('telefono');
 }
+
 
 function accederPerfil (){
-    if(email != undefined) {
-        document.addEventListener("DOMContentLoaded", function (){
+  if(email != undefined) {
+      document.addEventListener("DOMContentLoaded", function (){
 
- let htmlContentToAppend = "";
-    
-    htmlContentToAppend += `
-    <div class="p-4">  
-        <h2 class="mt-5">Perfil</h2>
-        <hr>
-        <form id='infoPerfil' class="needs-validation" novalidate>
-          <div class="row d-flex justify-content-center">
-            <div class=" col-lg-3 col-md-6 col-sm-12 my-3">
-              <label>Nombre*</label><br>
-              <input required>
-              <div class="invalid-feedback">
-            Debe ingresar nombre.
-          </div>
-              </div>
-              <div class="col-lg-3 col-md-6 col-sm-12 my-3">
-              <label>Segundo nombre</label><br>
-              <input>
+let htmlContentToAppend = "";
+  
+  htmlContentToAppend += `
+  <div class="p-4">  
+      <h2 class="mt-5">Perfil</h2>
+      <hr>
+      <form id='infoPerfil' class="needs-validation" novalidate>
+        <div class="row d-flex justify-content-center">
+          <div class=" col-lg-3 col-md-6 col-sm-12 my-3">
+            <label>Nombre*</label><br>
+            <input id='nombre' required>
+            <div class="invalid-feedback">
+          Debe ingresar nombre.
+        </div>
             </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 my-3">
+            <label>Segundo nombre</label><br>
+            <input id='segundoNombre'>
           </div>
-          <div class="row d-flex justify-content-center">
-            <div class=" col-lg-3 col-md-6 col-sm-12 my-3">
-              <label>Apellido*</label><br>
-              <input required>
-              </div>
-              <div class="col-lg-3 col-md-6 col-sm-12 my-3">
-              <label>Segundo apellido</label><br>
-              <input>
+        </div>
+        <div class="row d-flex justify-content-center">
+          <div class=" col-lg-3 col-md-6 col-sm-12 my-3">
+            <label>Apellido*</label><br>
+            <input id='apellido'required>
+            <div class="invalid-feedback">
+          Debe ingresar apellido.
+        </div>
             </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 my-3">
+            <label>Segundo apellido</label><br>
+            <input id='segundoApellido'>
           </div>
-          <div class="row d-flex justify-content-center">
-            <div class=" col-lg-3 col-md-6 col-sm-12 my-3">
-              <label>E-mail*</label><br>
-              <input id="email" required>
-              </div>
-              <div class="col-lg-3 col-md-6 col-sm-12 my-3">
-              <label>Teléfono de contacto</label><br>
-              <input>
-            </div> 
+        </div>
+        <div class="row d-flex justify-content-center">
+          <div class=" col-lg-3 col-md-6 col-sm-12 my-3">
+            <label>E-mail*</label><br>
+            <input id="email" required>
+            <div class="invalid-feedback">
+          Debe ingresar e-mail.
+        </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12 my-3">
+            <label>Teléfono de contacto</label><br>
+            <input id='telefono'>
+          </div> 
+        </div>
+        <div class="row"> 
+        <div class="col-10">
+        <input type="submit" class="btn btn-outline-dark" id="guardarCambios" value='Guardar cambios'> 
           </div>
-          <div class="row"> 
-          <div class="col-10">
-            <input type="submit" class="btn btn-outline-dark" id="guardarCambios" value='Guardar cambios'> 
-            </div>
-            </div>
+          </div>
 
-        </form>
-      </div> `    
-    document.getElementById("formularioPerfil").innerHTML = htmlContentToAppend;
+      </form>
+    </div> `    
+  document.getElementById("formularioPerfil").innerHTML = htmlContentToAppend;
 
-    ponerEmail();
+  ponerDatos();
 
-    document.getElementById('guardarCambios').addEventListener('click', function(e) {    
-        alert('holi');
-        validaciones(e);
+  document.getElementById('guardarCambios').addEventListener('click', function(e) {    
+    validaciones(e);
+  })
       })
-
-        })
-    } else {
-        document.getElementById('alertaPerfil').classList.add('show');
-    }}
-
-accederPerfil()
+  } else {
+      document.getElementById('alertaPerfil').classList.add('show');
+  }}
 
 
-
-
+ 
 function validaciones(evento) {
   
-    if (!infoPerfil.checkValidity()) {
-      evento.preventDefault()
-      evento.stopPropagation()
-    } else{
-      alert("exito, ahora solo te falta guardar emi")
-    }
-    infoPerfil.classList.add('was-validated');
-      };
+  if (!infoPerfil.checkValidity()) {
+    evento.preventDefault()
+    evento.stopPropagation()
+  } else{
+    guardarInfo ()
+  }
+  infoPerfil.classList.add('was-validated');
+    };
 
 
-function guardarInfo (){
-
-}
+    
+function guardarInfo () {
+  let nombre= document.getElementById("nombre").value ;
+  let segundoNombre= document.getElementById("segundoNombre").value ;
+  let apellido = document.getElementById("apellido").value
+  let segundoApellido= document.getElementById("segundoApellido").value ;
+  let telefono= document.getElementById("telefono").value ;
   
+  localStorage.setItem('nombre', nombre);
+  localStorage.setItem('apellido', apellido);
+  localStorage.setItem('segundoNombre', segundoNombre);
+  localStorage.setItem('segundoApellido', segundoApellido);
+  localStorage.setItem('telefono', telefono);
+  
+}
+
+
+accederPerfil();
+
+
 
 
